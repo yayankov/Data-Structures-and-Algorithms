46515e2b51a4b70e78b4aa7396f66eebca4867a0
yayankov <52356711+yayankov@users.noreply.github.com>
2020-09-16 18:17:51 +0300

    Create SDAmisson.cpp

---
+++ FMI Tasks/SDAmisson.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int M;
+    cin>>M;
+    int N;
+    cin>>N;
+    int A[N];
+    for(int i=0;i<N;i++)
+    {
+        cin>>A[i];
+    }
+    long long ctrPlus=0;
+    long long ctrMinus=0;
+    for(int i=0;i<N;++i)
+    {
+        if(A[i]>M)
+        {
+            while(A[i]>M)
+            {
+                ctrPlus++;
+                A[i]--;
+            }
+        } else if(A[i]<M)
+        {
+            while(A[i]<M)
+            {
+                ctrMinus--;
+                A[i]++;
+            }
+        }
+
+    }
+    if(ctrPlus+ctrMinus>=0)
+        {
+          cout<<"yes";
+        }
+    else cout<<"no";
+    return 0;
+}
+
