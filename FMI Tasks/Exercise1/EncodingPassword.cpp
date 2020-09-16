158f1fbb842fb65770cc3ab957782ae7fc0e899c
yayankov <52356711+yayankov@users.noreply.github.com>
2020-09-16 18:27:09 +0300

    Update EncodingPassword.cpp

--- FMI Tasks/Exercise1/EncodingPassword.cpp
+++ FMI Tasks/Exercise1/EncodingPassword.cpp
@@ -1,5 +1,3 @@
-![EncodingPassword](https://user-images.githubusercontent.com/52356711/93354323-8b329000-f845-11ea-9b98-a9c0327b63b8.JPG)
-
 #include<iostream>
 #include<string>
 using namespace std;
