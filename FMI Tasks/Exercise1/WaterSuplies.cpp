f2b786a9ffe836a26453bf2ff7f1904046484b12
yayankov <52356711+yayankov@users.noreply.github.com>
2020-09-17 15:51:55 +0300

    Update WaterSuplies.cpp

--- FMI Tasks/Exercise1/WaterSuplies.cpp
+++ FMI Tasks/Exercise1/WaterSuplies.cpp
@@ -6,66 +6,38 @@ using namespace std;
 int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
     int a[n];
     for(int i=0; i<n; i++)
     {
-        cin>>a[i];
+        cin >> a[i];
     }
-    int tempS=0;
-    int S=0;
-    int best2=0;
-    int best=0;
-    int road=0;
-    int road2=0;
+    int tempS = 0;
+    int S = 0;
+    int best2 = 0;
+    int best = 0;
+    int road = 0;
+    int road2 = 0;
     for(int i=0; i<n; i++)
     {
-      /*  if(best< a[i)+i )
+        for(int j=0; j<n; j++)
         {
-            best=a[i];
-            cout<<" best:"<<best<<endl;
-        }
-        if(best2<(a[i]+abs(i-road)))
-        {
-            best2=a[i];
-            road2=abs(i-road);
-        cout<<" best2:"<<best2<<endl;
-
-        cout<<" road:"<<road2<<endl;
-
-        }
-        if(best>best2)
-        {
-            tempS=best*road2;
-        }
-        else
-        {
-            tempS=best2*road2;
-
-        }
-        if(S<tempS)
-        {
-            S=tempS;
-        }*/
-
-        for(int j=0;j<n;j++)
-        {
-            road=abs(i-j);
-            if(a[i]>a[j])
+            road = abs(i-j);
+            if(a[i] > a[j])
             {
-                tempS=a[j]*road;
+                tempS = a[j]*road;
             }
             else{
-                tempS=a[i]*road;
+                tempS = a[i]*road;
             }
-            if(S<tempS)
+            if(S < tempS)
             {
-                S=tempS;
+                S = tempS;
             }
         }
 
     }
-    cout<<S;
+    cout << S;
 
     return 0;
 }
